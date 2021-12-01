60e74984a1507ccaccd3e521f355f1e7b6967e86
Almog Adziashvili <almoga@wix.com>
2021-12-01 07:10:27 +0200

    day 1

--- days/1/solve.ts
+++ days/1/solve.ts
@@ -1,9 +1,18 @@
 import { exampleInput, puzzleInput } from "./puzzle-input";
 import { assert } from 'chai';
 
-export const solve = (_arr: any[]): any => {
+export const solve1 = (_arr: any[]): any => {
 	const arr = [..._arr];
-	return arr;
+	return arr.filter((a, i, b) => i > 0 ? b[i - 1] < a : false).length;
+};
+
+export const solve2 = (_arr: any[]): any => {
+	const arr = [..._arr];
+	return arr
+		.filter((a, i, b) => i > 0
+			? (b[i - 1] + b[i - 2] + b[i - 3]) < (a + b[i - 1] + b[i - 2])
+			: false)
+		.length;
 };
 
 const processInput = (input: string): any => {
@@ -11,22 +20,35 @@ const processInput = (input: string): any => {
 		input.split('\n')
 			.map(n => n.trim())
 			.filter((v) => !!v)
-			// .map(Number);
-
+			.map(Number);
 	return n;
 };
 
-describe('Day', () => {
+describe.skip('Part 1', () => {
 	it('Test Case 1', () => {
-		const result = solve(processInput(exampleInput));
+		const result = solve1(processInput(exampleInput));
 		result; //?
-		assert.deepEqual(result, []);
+		assert.deepEqual(result, 7);
 	});
 
 	it('Result', () => {
-		const result = solve(processInput(puzzleInput));
+		const result = solve1(processInput(puzzleInput));
 		result; //?
-		assert.deepEqual(result, []);
+		assert.deepEqual(result, null);
+	});
+});
+
+describe.skip('Part 2', () => {
+	it('Test Case 1', () => {
+		const result = solve2(processInput(exampleInput));
+		result; //?
+		assert.deepEqual(result, 5);
+	});
+
+	it('Result', () => {
+		const result = solve2(processInput(puzzleInput));
+		result; //?
+		assert.deepEqual(result, null);
 	});
 });
 
